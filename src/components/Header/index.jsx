a0051d7ab25d3677f5316589a7071ba4155f896f
Moshood Abidemi <olalekanteeblaze@gmail.com>
2021-07-04 18:53:27 +0100

    Add header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useCallback } from 'react'
+import axios from 'axios'
+import logo from '../../images/logo.svg'
+import { debounce } from '../../utils'
+import './index.scss'
+
+
+const Header = ({ setMovies, searchQuery, setSearchQuery }) => {
+    const searchMovies = useCallback(
+       debounce(async (query) => {
+           if(query !== ''){
+                const res = await axios.get(`${process.env.REACT_APP_API_DOMAIN}/search/movie?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&query=${query}`)
+                if(res.status === 200){
+                    setMovies(res.data.results)
+                }
+           }
+    }, 500), [])
+    const handleInputChange = (e) => {
+        setSearchQuery(e.target.value)
+        searchMovies(e.target.value)
+    }
+    return (
+        <header className="header">
+            <img src={logo} alt="Timescale Logo" className="header__logo" />
+            <input type="text" placeholder="Search for a movie" className="header__input" value={searchQuery} onChange={handleInputChange}/>
+        </header>
+    )
+}
+
+export default Header;
