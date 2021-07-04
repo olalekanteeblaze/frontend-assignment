998bec00a33aa4b5af4c8a22209f09eb10bcbc81
Moshood Abidemi <olalekanteeblaze@gmail.com>
2021-07-04 18:54:30 +0100

    add placeholder image

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Header from './Header'
+import Movies from './Movies'
+import Modal from './Modal'
 
-const App = () => (
-	<img src={logo} alt="Timescale" />
-)
+const App = () => {
+	const [movies, setMovies] = useState([])
+	const [searchQuery, setSearchQuery] = useState('')
+	const [visible, setVisible] = useState(false)
+	const [selectedMovie, setSelectedMovie] = useState([])
+	async function fetchData() {
+		const res = await axios.get(`${process.env.REACT_APP_API_DOMAIN}/movie/popular?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}`)
+		if(res.status === 200){
+			setSearchQuery('')
+			setMovies(res.data.results)
+		}
+	}
+	useEffect(() => {
+		if(!searchQuery && movies.length === 0){
+			fetchData()
+		}
+	},[searchQuery, movies])
+	return (
+		<>
+			<Header setMovies={setMovies} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+			<Movies movies={movies} searchQuery={searchQuery} fetchData={fetchData} setSelectedMovie={setSelectedMovie} setVisible={setVisible}/>
+			<Modal visible={visible} setVisible={setVisible} movie={selectedMovie} />
+		</>
+	)
+}
 
 export default App;
---
+++ src/components/Movies/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Placeholder from '../../images/placeholder.png'
+import './index.scss'
+
+const Movies = ({ movies, searchQuery, fetchData, setSelectedMovie, setVisible }) => {
+    return (
+        <div className="movies-container">
+            <h3 className="movies-container__title">{ searchQuery ? `Showing results for '${searchQuery}'` : 'Most Recent Movies' }</h3>
+            {searchQuery && movies.length == 0 ? 
+            <div className="movies-container__empty-state">
+                <div>
+                    <div>{`No result  for ${searchQuery}`}</div>
+                    <button className="movies-container__btn btn" onClick={() => fetchData()}>
+                        Show Recent Movies
+                    </button>
+                </div>
+            </div> :
+            <div className="movies-container__grid">
+                {movies?.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} setSelectedMovie={setSelectedMovie} setVisible={setVisible} />
+                ))}
+            </div>
+            }
+        </div>
+    )
+}
+
+const MovieCard = ({ movie, setSelectedMovie, setVisible }) => {
+    const { title, vote_average, poster_path } = movie
+    const imgSrc = poster_path ?  `${process.env.REACT_APP_API_BASE_IMAGE_URL}${poster_path}` : Placeholder
+    const selectMovie = () => {
+        setVisible(true)
+        setSelectedMovie(movie)
+    }
+    return (
+        <div className="movie-card" onClick={selectMovie}>
+            <img src={imgSrc} alt={`Poster for ${title}`} className="movie-card__poster" loading="lazy"/>
+            <div className="movie-card__rating">{vote_average}</div>
+            <div className="movie-card__name">{title}</div>
+        </div>
+    )
+}
+export default Movies
